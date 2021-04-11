686e80265f7b6af945051cc949f87469eeda9d10
gregfolker <greg.folker@pm.me>
2021-04-11 13:59:10 -0500

    refactor: create implementation of `Config` and a `new()` method

--- src/main.rs
+++ src/main.rs
@@ -10,37 +10,39 @@ struct Config {
     filename: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Config {
+        assert_eq!(
+           args.len(), NUM_CLI_ARGS,
+            "Expected {} arguments, got {}",
+            (NUM_CLI_ARGS - 1), (args.len() - 1)
+        );
+
+        /*
+        * @note: The `clone()` method takes more time and memory
+        * than using a reference to the string data. However, the
+        * tradeoff is that it is more straightforward because now
+        * the lifetime of the references do not need to be managed.
+        * In this circumstance, giving up a little performance to
+        * gain simplicity is a worthwhile trade-off.
+        *
+        * There is a tendency for many Rust programmers to avoid
+        * using the `clone()` method because of the implications
+        * to performance. However, sometimes it is better to have
+        * a working program that is a little bit less efficient
+        * than try to hyperoptimize it.
+        */
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for '{}' in {}...", config.query, config.filename);
-}
-
-fn parse_config(args: &[String]) -> Config {
-    assert_eq!(
-        args.len(), NUM_CLI_ARGS,
-        "Expected {} arguments, got {}",
-        (NUM_CLI_ARGS - 1), (args.len() - 1)
-    );
-
-    /*
-     * @note: The `clone()` method takes more time and memory
-     * than using a reference to the string data. However, the
-     * tradeoff is that it is more straightforward because now
-     * the lifetime of the references do not need to be managed.
-     * In this circumstance, giving up a little performance to
-     * gain simplicity is a worthwhile trade-off.
-     *
-     * There is a tendency for many Rust programmers to avoid
-     * using the `clone()` method because of the implications
-     * to performance. However, sometimes it is better to have
-     * a working program that is a little bit less efficient
-     * than try to hyperoptimize it.
-     */
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
 }
