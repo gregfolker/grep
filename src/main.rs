2c5cfe3cad7fc8a3c4194f2bb1668327b7f6daf2
gregfolker <greg.folker@pm.me>
2021-05-09 17:25:09 -0500

    feat: print runtime error from main if encountered

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = grep::run(config) {
+        println!("Error: {}", e);
         process::exit(1);
     }
 }
